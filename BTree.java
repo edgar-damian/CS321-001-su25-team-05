f69fa9a5f6831158ba9d044dd392020b95c6567a
Edgar Rios Negrete <edgariosnegrete@icloud.com>
2025-08-01 09:39:18 -0600

    Created the file BTree.java

---
+++ BTree.java
@@ -0,0 +1,11 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.Random;
+
+public class BTree {
+
+}
